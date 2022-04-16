fab3467a35e6dbbb2a2a30fa5c32d3ec9bb528e3
dlswer23 <lbj4298@naver.com>
2022-04-16 23:29:53 +0900

    [UPDATE] certification class

---
+++ lib/login.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const login());
+}
+
+class login extends StatelessWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class certification extends StatelessWidget {
+  const certification({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
