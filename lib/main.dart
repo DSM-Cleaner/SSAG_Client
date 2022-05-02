a43237a7f7425e819c61503f17c108874ff7cbd2
dlswer23 <lbj4298@naver.com>
2022-05-02 09:07:55 +0900

    [UPDATE] clean code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ssag/sign.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,54 +9,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SSAG',
-      home: SignMain(),
+      home: SplashView(),
     );
   }
 }
 
-class Splash extends StatelessWidget {
-  const Splash({Key? key}) : super(key: key);
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [Text('쓰윽싹'), Text('DSM 청소 관리 어플')],
+    return MaterialApp(
+        home: Container(
+      color: Color.fromRGBO(123, 166, 231, 1),
+      child: Center(
+        child: Text(
+          "쓰윽싹",
+          style: TextStyle(color: Colors.white, fontSize: 30),
         ),
       ),
-    );
+    ));
   }
 }
 
-class SplashAnimation extends StatelessWidget {
-  const SplashAnimation({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // Figma Flutter Generator Splash_animationWidget - FRAME
-    return Container(
-        width: 360,
-        height: 800,
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(123, 166, 231, 1),
-        ),
-        child: Stack(children: <Widget>[
-          Positioned(
-              top: 366,
-              left: 113,
-              child: Text(
-                '쓰윽싹',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    fontFamily: 'Hansan',
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    fontSize: 40,
-                    letterSpacing:
-                        0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1),
-              )),
-        ]));
-  }
-}
